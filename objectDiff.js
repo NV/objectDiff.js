d365d8783e81bbbe520869b6f81444d15f6b383f
Nikita Vasilyev <me@elv1s.ru>
2011-10-27 02:44:41 +0400

    How did I wrote this? I guess I was out of my mind.

--- objectDiff.js
+++ objectDiff.js
@@ -92,7 +92,7 @@ repeatString.cache = {};
 function stringifyObjectKey(key) {
 	return /^[a-z_$][a-z0-9_$]*$/i.test(key) ?
 		key :
-		'"' + key.replace(/[^\\]"/, '\\"') + '"';
+		JSON.stringify(key);
 }
 
 
